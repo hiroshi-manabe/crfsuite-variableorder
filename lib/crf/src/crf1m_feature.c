f8044d12bfe601cb051b4e939ebe1245ba0f0314
Hiroshi MANABE <t_takeda25@hotmail.com>
2011-02-22 14:53:17 +0900

    Renamed and created a new configuration

--- lib/crf/src/crf1m_feature.c
+++ lib/crf/src/crf1m_feature.c
@@ -30,7 +30,6 @@
 
 /* $Id: crf1m_feature.c 176 2010-07-14 09:31:04Z naoaki $ */
 
-#include "esa.hxx"
 #include <vector>
 using namespace std;
 
@@ -185,187 +184,82 @@ crf1ml_features_t* crf1ml_generate_features(
     /* Loop over the sequences in the training data. */
     logging_progress_start(&lg);
 
-	if (emulate_crf1m) {
-		for (s = 0; s < N; ++s) {
-			int prev = L, cur = 0;
-			const crf_item_t* item = NULL;
-			const crf_sequence_t* seq = &seqs[s];
-			const int T = seq->num_items;
-			uint8_t* label_sequence = (uint8_t*)malloc((T+1) * sizeof(uint8_t));
-			label_sequence[T] = L; // BOS
+	for (s = 0; s < N; ++s) {
+		int prev = L, cur = 0;
+		const crf_item_t* item = NULL;
+		const crf_sequence_t* seq = &seqs[s];
+		const int T = seq->num_items;
+		uint8_t* label_sequence = (uint8_t*)malloc((T+1) * sizeof(uint8_t));
+		label_sequence[T] = L; // BOS
 
-			for (t = 0; t < T; ++t) {
-				int a;
-				int label_sequence_pos = T - t - 1;
-				item = &seq->items[t];
-				label_sequence[label_sequence_pos] = item->label;
+		for (t = 0; t < T; ++t) {
+			int a;
+			int label_sequence_pos = T - t - 1;
+			item = &seq->items[t];
+			label_sequence[label_sequence_pos] = item->label;
 
-				memset(&f, 0, sizeof(f));
-				memcpy(f.label_sequence, &label_sequence[label_sequence_pos], 2);
-				f.order = 2;
-				f.freq = 1;
-				f.attr = item->contents[0].aid;
-				featureset_add(set, &f);
-
-				f.attr = item->contents[1].aid;
-				featureset_add(set, &f);
-
-				f.attr = item->contents[2].aid;
-				featureset_add(set, &f);
-
-				if (t >= 1) {
-					f.label_sequence[2] = label_sequence[label_sequence_pos+2];
-					f.order = 3;
-					featureset_add(set, &f);
-					
-					f.attr = item->contents[4].aid;
-					featureset_add(set, &f);
-
-					f.attr = item->contents[6].aid;
-					featureset_add(set, &f);
-
-					if (t >= 2) {
-						f.label_sequence[3] = label_sequence[label_sequence_pos+3];
-						f.order = 4;
-						featureset_add(set, &f);
-					}
-				}
-
-				if (t < T-1) { // t == T-1 : EOS
-					for (a = 0; a < item->num_contents; ++a) {
-						memset(&f, 0, sizeof(f));
-						f.attr = item->contents[a].aid;
-						f.order = 1;
-						f.label_sequence[0] = label_sequence[label_sequence_pos];
-						f.freq = 1;
-						featureset_add(set, &f);
-					}
-				}
-			}
-			free(label_sequence);
-		}
-		for (i = 0; i < L; ++i) {
-			// BOS
 			memset(&f, 0, sizeof(f));
-			f.label_sequence[0] = i;
-			f.label_sequence[1] = L;
+			memcpy(f.label_sequence, &label_sequence[label_sequence_pos], 2);
 			f.order = 2;
-			f.freq = 0;
-			f.attr = 0;
+			f.freq = 1;
+			f.attr = item->contents[0].aid;
 			featureset_add(set, &f);
 
-			// EOS
-			memset(&f, 0, sizeof(f));
-			f.label_sequence[0] = L;
-			f.label_sequence[1] = i;
-			f.order = 2;
-			f.freq = 0;
-			f.attr = 0;
+			f.attr = item->contents[1].aid;
 			featureset_add(set, &f);
-		}
-	} else {
-		vector<int> label_vector;
-		vector<const crf_item_t*> item_pointer_vector;
 
-		for (s = 0; s < N; ++s) {
-			int prev = L, cur = 0;
-			const crf_item_t* item = NULL;
-			const crf_sequence_t* seq = &seqs[s];
-			const int T = seq->num_items;
+			f.attr = item->contents[2].aid;
+			featureset_add(set, &f);
 
-			/* BOS */
-			label_vector.push_back(L);
-			item_pointer_vector.push_back(0);
+			if (t >= 1) {
+				f.label_sequence[2] = label_sequence[label_sequence_pos+2];
+				f.order = 3;
+				featureset_add(set, &f);
+				
+				f.attr = item->contents[4].aid;
+				featureset_add(set, &f);
 
-			/* Loop over the items in the sequence. */
-			for (t = 0;t < T;++t) {
-				item = &seq->items[t];
-				label_vector.push_back(item->label); // t == T-1 : EOS
-				item_pointer_vector.push_back(item);
+				f.attr = item->contents[6].aid;
+				featureset_add(set, &f);
+
+				if (t >= 2) {
+					f.label_sequence[3] = label_sequence[label_sequence_pos+3];
+					f.order = 4;
+					featureset_add(set, &f);
+				}
 			}
 
-			logging_progress(&lg, s * 100 / N);
-		}
-		logging_progress_end(&lg);
-
-		int size = label_vector.size();
-		vector<int> SA(size);
-		vector<int> left (size);
-		vector<int> right (size);
-		vector<int> D (size);
-		int k = L + 1;
-		int nodeNum = 0;
-		int ret = esaxx(label_vector.begin(), SA.begin(), 
-			left.begin(), right.begin(), D.begin(), 
-			(int)label_vector.size(), k, nodeNum);
-
-		vector<int> rank (size);
-		int r = 0;
-		for (i = 0; i < size; i++) {
-			if (i == 0 || SA[i] == 0 || SA[i-1] == 0 || label_vector[(SA[i] + size - 1) % size] != label_vector[(SA[i - 1] + size - 1) % size]) r++;
-			rank[i] = r;
-		}
-
-		int* attribute_count_array = new int[num_attributes];
-		int* appeared_attributes_array = new int[num_attributes];
-		memset(attribute_count_array, 0, num_attributes * sizeof(int));
-
-		for (i = 0; i < nodeNum; i++) {
-			if (rank[right[i] - 1] - rank[left[i]] > 0 && D[i] <= MAX_ORDER) {
-				int length = D[i];
-				if (length == 0) continue;
-				char label_sequence[MAX_ORDER] = {0};
-				int l = SA[left[i]];
-				int r = SA[left[i]] + length - 1;
-
-				int current_position, last_position;
-				current_position = last_position = r;
-				bool is_first = true;
-
-				while (last_position >= l) {
-					for (current_position = last_position;
-						current_position > l &&
-						!(current_position > 0 &&
-						  label_vector[current_position] == L &&
-						  label_vector[current_position-1] == L
-						);
-						--current_position);
-					is_first = false;
-					current_position--;
-					int order = last_position - current_position;
-
-					for (j = 0; j < order; ++j) {
-						label_sequence[j] = label_vector[last_position - j]; // copy label sequence(reversed)
-					}
-					int   appeared_attributes_count = 0;
-					for (j = left[i]; j < right[i]; ++j) {
-						const crf_item_t* item = item_pointer_vector[SA[j] + last_position - l];
-						if (item == NULL) continue; // BOS
-						for (c = 0; c < item->num_contents; c++) {
-							int aid = item->contents[c].aid;
-							if (attribute_count_array[aid] == 0) {
-								appeared_attributes_array[appeared_attributes_count] = aid;
-								appeared_attributes_count++;
-							}
-							attribute_count_array[aid]++;
-						}
-					}
-					for (j = 0; j < appeared_attributes_count; ++j) {
-						if (attribute_count_array[appeared_attributes_array[j]] > 1) {
-							f.order = order;
-							f.freq = attribute_count_array[appeared_attributes_array[j]];
-							f.attr = appeared_attributes_array[j];
-							memcpy(f.label_sequence, label_sequence, MAX_ORDER * sizeof(char));
-							featureset_add(set, &f);
-						}
-						attribute_count_array[appeared_attributes_array[j]] = 0;
-					}
-					last_position = current_position;
+			if (t < T-1) { // t == T-1 : EOS
+				for (a = 0; a < item->num_contents; ++a) {
+					memset(&f, 0, sizeof(f));
+					f.attr = item->contents[a].aid;
+					f.order = 1;
+					f.label_sequence[0] = label_sequence[label_sequence_pos];
+					f.freq = 1;
+					featureset_add(set, &f);
 				}
 			}
 		}
-		delete[] attribute_count_array;
-		delete[] appeared_attributes_array;
+		free(label_sequence);
+	}
+	for (i = 0; i < L; ++i) {
+		// BOS
+		memset(&f, 0, sizeof(f));
+		f.label_sequence[0] = i;
+		f.label_sequence[1] = L;
+		f.order = 2;
+		f.freq = 0;
+		f.attr = 0;
+		featureset_add(set, &f);
+
+		// EOS
+		memset(&f, 0, sizeof(f));
+		f.label_sequence[0] = L;
+		f.label_sequence[1] = i;
+		f.order = 2;
+		f.freq = 0;
+		f.attr = 0;
+		featureset_add(set, &f);
 	}
 
     /* Convert the feature set to an feature array. */
